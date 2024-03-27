450bba9b49065be5dba98574f5eaf2362ef75b8f
Maxx Lehmann <mglehman@mtu.edu>
2024-03-27 00:10:16 -0600

    start and stop with time delta and proper db file location

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use clap::{Parser, Subcommand};
-use sqlx::{migrate::MigrateDatabase, FromRow, Pool, Row, Sqlite, SqlitePool};
-use chrono::{DateTime, Local};
+use std::{fmt::Debug, fs::create_dir, io::ErrorKind, io::Write};
 
-const DB_URL: &str = "sqlite://sqlite.db";
+use clap::{Parser, Subcommand};
+use directories::ProjectDirs;
+use sqlx::{migrate::MigrateDatabase, FromRow, Pool, Sqlite, SqlitePool};
+use chrono::{DateTime, Local, TimeDelta};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 #[derive(Clone, FromRow, Debug)]
 struct Shift {
-    id: i64,
     time_in: i64,
-    time_out: Option<i64>
+    time_out: Option<i64>,
+    time_diff: Option<i64>,
 }
 
 fn format_timestamp(t: i64) -> String {
@@ -16,6 +18,30 @@ fn format_timestamp(t: i64) -> String {
     let tz = *Local::now().offset();
     let dtz = dt.with_timezone(&tz);
     return dtz.format("%I:%M%P on %A, %b %d %Y").to_string();
+}
+
+fn format_timediff(t: i64) -> String {
+    let delta = TimeDelta::new(t, 0).unwrap();
+    let hours = delta.num_hours();
+    let minutes = delta.num_minutes() % 60;
+    return format!("{hours:0>2}:{minutes:0>2}");
+}
+
+fn print_target_delta(t: i64) {
+    let delta = TimeDelta::new(t - 32400, 0).unwrap();
+    let is_neg = delta.num_minutes() < 0;
+    let abs_delta = delta.abs();
+    let hours = abs_delta.num_hours();
+    let minutes = abs_delta.num_minutes() % 60;
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let formatted = format!("({hours:0>2}:{minutes:0>2})");
+
+    if is_neg {
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red))).unwrap();
+    } else {
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
+    }
+    writeln!(&mut stdout, "{formatted}").unwrap();
 }
 
 // Simple program to greet a person
@@ -65,24 +91,28 @@ enum Commands {
 }
 
 async fn get_database() -> Pool<Sqlite>{
-    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
-        println!("Creating database {}", DB_URL);
-        match Sqlite::create_database(DB_URL).await {
-            Ok(_) => println!("Create db success"),
-            Err(error) => panic!("error: {}", error),
-        }
+    let proj_dirs = ProjectDirs::from("", "",  "track-cli").unwrap();
+    let data_dir = proj_dirs.data_dir().to_str().unwrap();
+    match create_dir(data_dir) {
+        Ok(_) => (),
+        Err(err) if err.kind() == ErrorKind::AlreadyExists => (),
+        Err(err) => panic!("Data directory could not be created: {err}"),
+    }
+    let db_url = &format!("sqlite://{data_dir}/sqlite.db");
+
+    if !Sqlite::database_exists(db_url).await.unwrap_or(false) {
+        println!("Creating database {}", db_url);
+        Sqlite::create_database(db_url).await.unwrap();
     }
 
-    let db = SqlitePool::connect(DB_URL).await.unwrap();
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let db = SqlitePool::connect(db_url).await.unwrap();
+    let crate_dir = env!("CARGO_MANIFEST_DIR");
     let migrations = std::path::Path::new(&crate_dir).join("./migrations");
-    let migration_results = sqlx::migrate::Migrator::new(migrations)
+    if let Err(error) = sqlx::migrate::Migrator::new(migrations)
         .await
         .unwrap()
         .run(&db)
-        .await;
-
-    if let Err(error) = migration_results {
+        .await {
         panic!("error: {}", error);
     }
 
@@ -106,6 +136,17 @@ async fn main() {
     
     match &cli.command {
         Some(Commands::Start {  }) => {
+            let open_shift_count = sqlx::query_scalar::<_, i64>("
+                SELECT COUNT(*) FROM shifts WHERE time_out IS NULL
+            ")
+                .fetch_one(&db)
+                .await
+                .unwrap();
+            if open_shift_count > 0 {
+                eprintln!("A shift is already in progress");
+                return;
+            }
+
             let result = sqlx::query_as::<_, Shift>("
                 INSERT INTO shifts (id) VALUES (NULL) RETURNING *;
             ")
@@ -116,7 +157,7 @@ async fn main() {
             println!("Shift started at {}", timestamp);
         }
         Some(Commands::Stop {  }) => {
-            let result = sqlx::query("
+            let result = sqlx::query_as::<_, Shift>("
                 UPDATE shifts
                 SET time_out = (unixepoch()),
                     time_diff = (unixepoch() - time_in)
@@ -128,12 +169,13 @@ async fn main() {
                 .await;
 
             match result {
-                Ok(row) => {
-                    println!("{:?}", row.columns());
-                    println!("{:?}", row.get::<i64, &str>("now"));
-                    println!("{:?}", row.get::<String, usize>(2));
-                    // let timestamp = format_timestamp(row.time_in);
-                    // println!("Shift stopped at {}, hours worked: {}", timestamp, timestamp);
+                Ok(shift) => {
+                    let timestamp = format_timestamp(shift.time_out.unwrap());
+                    let hours_worked = format_timediff(shift.time_diff.unwrap());
+
+                    println!("Shift stopped at {timestamp}");
+                    print!("hours worked: {hours_worked} ");
+                    print_target_delta(shift.time_diff.unwrap());
                 },
                 Err(error) => {
                     if error.to_string().starts_with("no rows") {
