38eb1d524271d52cf6638333b16f88b4012ed5cd
Maxx Lehmann <mglehman@mtu.edu>
2024-04-14 22:11:56 -0600

    install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+set -e
+
+echo "Installing..."
+
+NAME=track
+INSTALL_DIR="/usr/local/bin"
+SYSTEM=$(uname -a)
+
+if [[ $SYSTEM == "Linux"* ]]; then
+  curl -sfL https://github.com/maxxgl/track/releases/download/prerelease/track-cli-linux > $INSTALL_DIR/$NAME
+elif [[ $SYSTEM == "Darwin"* ]]; then
+  curl -sfL https://github.com/maxxgl/track/releases/download/prerelease/track-cli-mac > $INSTALL_DIR/$NAME
+else
+  echo "Installation failed, system not supported"
+  echo "Supported systems:"
+  echo "x86: Mac, Linux"
+fi
+
+chmod 755 $INSTALL_DIR/$NAME
+
+echo "Track CLI Tool Successfully Installed"
